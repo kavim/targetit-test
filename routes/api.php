d8efd57bc650fc792eaf91692f8c49b477e67eed
Kevin M. Medeiros <kmedeiros@alephee.com>
2025-07-28 07:57:37 -0300

    feat: add permissions update route to API

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('users', UserController::class);
     Route::post('addresses', [AddressController::class, 'store'])->name('addresses.store');
+    Route::post('permissions', [AddressController::class, 'update'])->name('permissions.update');
 });
