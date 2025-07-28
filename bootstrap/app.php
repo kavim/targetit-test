4d8077a35e8cdad2761f4756548218767fd84a43
Kevin M. Medeiros <kmedeiros@alephee.com>
2025-07-28 07:59:45 -0300

    chore: update application configuration to include AuthServiceProvider and middleware for API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Providers\AuthServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        AuthServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->appendToGroup('api', EnsureFrontendRequestsAreStateful::class);
+        $middleware->appendToGroup('api', 'throttle:api');
+        $middleware->appendToGroup('api', SubstituteBindings::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
