da23c87d69ba56781892b2a8c9ff5bdf8430d151
Kevin M. Medeiros <kmedeiros@alephee.com>
2025-07-28 07:57:06 -0300

    feat: enhance UserFactory and DatabaseSeeder with admin role and additional fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -26,10 +27,21 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone'    => $this->faker->phoneNumber(),
+            'cpf'      => $this->faker->unique()->numerify('###.###.###-##'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin()
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->permissions()->create([
+                'permission' => 'admin',
+            ]);
+        });
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->admin()
+            ->create([
+                'name'     => 'Super Admin',
+                'email'    => 'admin@example.com',
+                'phone'    => '51999999999',
+                'cpf'      => '000.000.000-00',
+                'password' => Hash::make('admin123'),
+            ]);
     }
 }
